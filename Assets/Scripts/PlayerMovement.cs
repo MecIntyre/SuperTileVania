75f8aa9bcde00fc7ad7addf9151115961efa2528
MecIntyre <abuesswalker@gmail.com>
2022-01-11 16:01:44 +0100

    implement jumping

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpSpeed = 20f;
 
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
@@ -26,6 +27,14 @@ public class PlayerMovement : MonoBehaviour
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (value.isPressed)
+        {
+            myRigidbody.velocity += new Vector2 (0f, jumpSpeed);
+        }
     }
 
     void Run()
