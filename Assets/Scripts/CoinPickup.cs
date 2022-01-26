6b85ae8effa3da5a8751dae43d7c23369d7b2c45
MecIntyre <abuesswalker@gmail.com>
2022-01-26 15:58:30 +0100

    add audio source

--- Assets/Scripts/CoinPickup.cs
+++ Assets/Scripts/CoinPickup.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    [SerializeField] AudioClip coinPickUpSFX;
     
     void OnTriggerEnter2D (Collider2D other)
     {
         if (other.tag == "Player")
         {
+            AudioSource.PlayClipAtPoint(coinPickUpSFX, Camera.main.transform.position);
             Destroy(gameObject);
         }
     }
